810a240c7cb0bb0c37744e8dd4fd4a1e83ec4061
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2024-10-21 21:09:13 -0400

    New Switch Project Skeleton with AlamoFire

---
+++ BetterApp_FinalProject/BetterApp_FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterApp_FinalProject
+//
+//  Created by Isabel Cuddihy on 10/21/24.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
